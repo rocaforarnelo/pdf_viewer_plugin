204a2c437da0216e10f6b29197d13132bb3d0ba9
F <arnelo.rocafor@paymaya.com>
2024-08-08 16:59:59 +0800

    update reference

--- android/src/main/java/dev/britto/pdf_viewer_plugin/PdfViewer.java
+++ android/src/main/java/dev/britto/pdf_viewer_plugin/PdfViewer.java
@@ -1,6 +1,6 @@
 package dev.britto.pdf_viewer_plugin;
 
-import com.github.mhiew.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.PDFView;
 
 import android.content.Context;
 import android.util.AttributeSet;
